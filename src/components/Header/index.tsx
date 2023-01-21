6cbc32975ec3d676da884faf60f8e0a5505eb59f
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-21 15:05:51 -0300

    feat(header): change h1 text

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export function Header({}: Props) {
   return (
     <HeaderSection>
       <div className="title">
-        <h1>Talespire Cards</h1>
+        <h1>Task Manager</h1>
       </div>
       <div className="theme">
         <ThemeButton />
