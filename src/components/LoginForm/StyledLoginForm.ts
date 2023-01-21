15e3cc94b3bc69611d4a1182abf65b5fc9403564
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-21 15:06:24 -0300

    style(form): change form style

--- src/components/LoginForm/StyledLoginForm.ts
+++ src/components/LoginForm/StyledLoginForm.ts
@@ -17,33 +17,25 @@ export const StyledLoginForm = styled.form`
   .form-wrapper {
     position: relative;
     display: flex;
-    width: 21rem;
+    width: 18rem;
     align-items: center;
     justify-content: space-between;
     margin: 0.4rem 0;
-  }
 
-  > div {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 15rem;
-    flex-direction: row;
-  }
+    div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 2.5rem;
+    }
 
-  svg {
-    vertical-align: middle;
-    margin-right: 0.2rem;
-  }
-
-  label {
-    font-size: var(--font-size-sm);
-    text-align: start;
-    vertical-align: middle;
+    svg {
+      vertical-align: middle;
+    }
   }
 
   input {
-    width: 14rem;
+    width: 15rem;
     padding: 0.5rem;
     border-radius: 12px;
     flex-basis: 1rem;
@@ -71,16 +63,16 @@ export const StyledLoginForm = styled.form`
 
   .form-input-password {
     position: absolute;
-    right: 3px;
+    right: 9px;
     cursor: pointer;
   }
 `
 export const StyledLoginSection = styled.section`
   position: relative;
   border-radius: 12px;
-  box-shadow: 0px 0px 3px 1px ${({ theme }) => theme.colors.secondary};
-  margin: 8em auto 2em auto;
+  margin: 9.8em auto 2em auto;
   padding: 1em 0 0 0;
+  /* box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.secondary}; */
 
   height: 30rem;
   width: var(--container-width-lg);
