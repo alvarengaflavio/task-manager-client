0e52899b202b6fa65fed923e1788ae6fb7ba6ca0
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-22 09:30:54 -0300

    feat(button): add style prop to Button

--- src/components/shared/Button/index.tsx
+++ src/components/shared/Button/index.tsx
@@ -1,25 +1,30 @@
+import { type } from 'os'
 import { StyledPrimaryButton, StyledSecondaryButton } from './StyledButton'
 
 type Props = {
   text?: string
   handleClick?: () => void
+  type?: 'submit' | 'button' | 'reset'
   style?: 'primary' | 'secondary'
 }
 
 export function Button({
+  handleClick = () => {},
   text = 'Button',
-  handleClick = () => {
-    console.log('Clicked')
-  },
+  type = 'button',
   style = 'primary',
 }: Props) {
   if (style === 'primary') {
     return (
-      <StyledPrimaryButton onClick={handleClick}>{text}</StyledPrimaryButton>
+      <StyledPrimaryButton type={type} onClick={handleClick}>
+        {text}
+      </StyledPrimaryButton>
     )
   }
 
   return (
-    <StyledSecondaryButton onClick={handleClick}>{text}</StyledSecondaryButton>
+    <StyledSecondaryButton onClick={handleClick} type={type}>
+      {text}
+    </StyledSecondaryButton>
   )
 }
