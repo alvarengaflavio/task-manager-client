782776ea34d81ee24c5bce148dde4ee7ab25a568
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-23 11:32:26 -0300

    style(error): change color & button & icon

--- src/utils/error/handle-error-modal.ts
+++ src/utils/error/handle-error-modal.ts
@@ -2,10 +2,20 @@ import Swal from 'sweetalert2'
 
 export function HandleError(err: Error, title?: string) {
   console.log(err)
+
   Swal.fire({
     title: title || 'Error',
     text: err.message,
-    icon: 'error',
-    confirmButtonText: 'Ok',
+    showConfirmButton: false,
+    background: '#1e1e1e',
+    color: '#afbec4',
+    timer: 2000,
   })
 }
+
+// Swal.fire({
+//   title: title || 'Error',
+//   text: err.message,
+//   icon: 'error',
+//   confirmButtonText: 'Ok',
+// })
