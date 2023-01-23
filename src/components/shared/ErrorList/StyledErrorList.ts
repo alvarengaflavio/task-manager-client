a5c7e4a0d3352f9c549f902ff07e771e831c6f4d
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-23 17:45:19 -0300

    style(ErrorList): add styled component `ErrorList`

--- src/components/shared/ErrorList/StyledErrorList.ts
+++ src/components/shared/ErrorList/StyledErrorList.ts
@@ -1,3 +1,28 @@
 import styled from 'styled-components'
 
-export const StyledErrorList = styled.div``
+export const StyledErrorList = styled.div`
+  border-radius: 9px;
+  margin: 2rem auto 0 auto;
+  width: 70%;
+
+  font-size: var(--font-size-sm);
+  color: var(--color-white);
+  background-color: ${({ theme }) => theme.colors.inlineError};
+  transition: var(--transition);
+
+  ul,
+  li,
+  p {
+    font-size: var(--font-size-sm);
+  }
+
+  ul {
+    color: var(--color-white);
+    padding: 1rem 1rem 1rem 2rem;
+    line-height: 1.2;
+  }
+
+  p {
+    color: var(--color-white);
+  }
+`
