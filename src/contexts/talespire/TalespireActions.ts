eeb24486bc39362f62263b0549367b91a1970972
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-23 11:17:54 -0300

    feat(action): implement `login` & add `register`

--- src/contexts/talespire/TalespireActions.ts
+++ src/contexts/talespire/TalespireActions.ts
@@ -2,7 +2,8 @@ import { HandleError } from '@/utils/error/handle-error-modal'
 import { LoginPayload } from '@/utils/types/payloads'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://taskmanager.sa-east-1.elasticbeanstalk.com/'
+axios.defaults.baseURL =
+  'http://taskmanager-alva.sa-east-1.elasticbeanstalk.com'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 axios.interceptors.request.use(
@@ -39,10 +40,29 @@ axios.interceptors.response.use(
 
 export const login = async ({ username, password }: LoginPayload) => {
   try {
-    const response = await axios.post('/auth/signin', {
-      username,
-      password,
-    })
+    const response = await axios.post('/auth/signin', { username, password })
+
+    if (response?.status === 201) {
+      console.log(response.data)
+      localStorage.setItem('accessToken', response.data.accessToken)
+
+      return response.data
+    }
+
+    throw new Error('Username or password is incorrect')
+  } catch (err: any) {
+    HandleError(err, 'Login Error')
+  }
+}
+
+export const register = async ({ username, password }: LoginPayload) => {
+  try {
+    const response = await axios.post(
+      '/auth/signup',
+      JSON.stringify({ username, password })
+    )
+
+    console.log(response.data)
     localStorage.setItem('accessToken', response.data.accessToken)
 
     return response.data
