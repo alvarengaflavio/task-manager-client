7c287e809dd7acdbb46cc8188f965e7e2b182967
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-24 20:10:44 -0300

    feat(taskFilter): add `TalespireContext` statusFilter & handleStatusFilter

--- src/components/shared/TaskFilter/index.tsx
+++ src/components/shared/TaskFilter/index.tsx
@@ -1,15 +1,47 @@
+import { TalespireContext } from '@/contexts/talespire/TalespireContext'
+import { StatusFilter } from '@/utils/types/tasks'
+import { useContext } from 'react'
 import { Button } from '../Button'
 import { StyledTaskFilter } from './StyledTaskFilter'
 
 export function TaskFilter({}: Props) {
+  const { statusFilter, handleStatusFilter } = useContext<FilterState>(
+    TalespireContext as any
+  )
+
+  const handleClick = (filter: StatusFilter) => {
+    handleStatusFilter(filter)
+  }
+
   return (
     <StyledTaskFilter>
-      <Button text="All" />
-      <Button text="Open" />
-      <Button text="In Progress" />
-      <Button text="Done" />
+      <Button
+        text="All"
+        handleClick={() => handleClick('ALL')}
+        className={statusFilter === 'ALL' ? 'active' : ''}
+      />
+      <Button
+        text="Open"
+        handleClick={() => handleClick('OPEN')}
+        className={statusFilter === 'OPEN' ? 'active' : ''}
+      />
+      <Button
+        text="In Progress"
+        handleClick={() => handleClick('IN_PROGRESS')}
+        className={statusFilter === 'IN_PROGRESS' ? 'active' : ''}
+      />
+      <Button
+        text="Done"
+        handleClick={() => handleClick('DONE')}
+        className={statusFilter === 'DONE' ? 'active' : ''}
+      />
     </StyledTaskFilter>
   )
 }
 
 type Props = {}
+
+type FilterState = {
+  statusFilter: StatusFilter
+  handleStatusFilter: (filter: StatusFilter) => void
+}
