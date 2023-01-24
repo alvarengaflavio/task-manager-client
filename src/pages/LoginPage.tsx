413b3c83d0d99318486fa1b522f9578c1a741132
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-23 21:32:40 -0300

    feat(loginPage): remove about button

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,28 +2,17 @@ import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { LoginForm } from '@/components/LoginForm'
 import { LoginTitle } from '@/components/LoginTitle'
-import { Button } from '@/components/shared/Button'
 import { ScreenWrapper } from '@/components/styles/StyledScreenWrapper'
-import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
 
 export function LoginPage({}: Props) {
-  const navigate = useNavigate()
-
-  const NavtoAbout = () => {
-    navigate('/about')
-  }
-
   return (
     <>
       <ScreenWrapper>
         <Header />
+
         <LoginTitle />
-        <LoginForm>
-          <ButtonsWrapper>
-            <Button style="secondary" text="About" handleClick={NavtoAbout} />
-          </ButtonsWrapper>
-        </LoginForm>
+        <LoginForm />
+
         <Footer />
       </ScreenWrapper>
     </>
@@ -31,8 +20,3 @@ export function LoginPage({}: Props) {
 }
 
 type Props = {}
-
-const ButtonsWrapper = styled.div`
-  margin: 11.5rem auto 0 auto;
-  width: max-content;
-`
