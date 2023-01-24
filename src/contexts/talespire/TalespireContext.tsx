190fc6bc33ea9e3a7219b928aed1af56c1999e26
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-24 20:13:16 -0300

    feat(context): add `statusFilter` state & handleStatusFilter

--- src/contexts/talespire/TalespireContext.tsx
+++ src/contexts/talespire/TalespireContext.tsx
@@ -1,17 +1,29 @@
+import { StatusFilter } from '@/utils/types/tasks'
 import { createContext, useReducer } from 'react'
-import { talespireReducer } from './TalespireReducer'
+import { talespireReducer, TalespireState } from './TalespireReducer'
 
 export const TalespireContext = createContext({})
 
 export const TalespireProvider = ({ children }: any) => {
-  const initialState = {
+  const initialState: TalespireState = {
     darkMode: true,
+    statusFilter: 'ALL',
   }
 
   const [state, dispatch] = useReducer(talespireReducer, initialState)
 
+  const handleDarkMode = () => {
+    dispatch({ type: 'darkMode', payload: !state.darkMode })
+  }
+
+  const handleStatusFilter = (status: StatusFilter) => {
+    dispatch({ type: 'statusFilter', payload: status })
+  }
+
   return (
-    <TalespireContext.Provider value={{ ...state, dispatch }}>
+    <TalespireContext.Provider
+      value={{ ...state, handleDarkMode, handleStatusFilter }}
+    >
       {children}
     </TalespireContext.Provider>
   )
