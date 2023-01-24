34b129903385fc6614e90b42c64f18492ff39db4
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-24 16:46:01 -0300

    feat(taskmenu): create task Menu

---
+++ src/components/TaskMenu/StyledTaskMenu.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import { StyledCard } from '../styles/StyledCard'
+
+export const StyledTaskMenu = styled(StyledCard)`
+  width: 90%;
+  max-width: 50rem;
+  height: 16rem;
+
+  h4 {
+    font-size: var(--font-size-xl);
+    font-weight: 300;
+  }
+`
