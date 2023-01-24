1fb3de4d0f0c28b282d2ccbb422fef5c449775f5
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-24 17:58:43 -0300

    styled(menu): fix magin

--- src/components/TaskMenu/StyledTaskMenu.ts
+++ src/components/TaskMenu/StyledTaskMenu.ts
@@ -6,6 +6,8 @@ export const StyledTaskMenu = styled(StyledCard)`
   max-width: 50rem;
   height: 16rem;
 
+  margin: 15rem auto 0 auto;
+
   h4 {
     font-size: var(--font-size-xl);
     font-weight: 300;
