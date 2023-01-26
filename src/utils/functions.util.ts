23376d9561187a181f3237e1ec7e4f6f321f6a47
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-25 23:01:30 -0300

    feat(utils): create getOrderedTasks to sort the TaskList

--- src/utils/functions.util.ts
+++ src/utils/functions.util.ts
@@ -1,1 +1,16 @@
+import { Task } from './types/tasks'
+
 export const doubleDigit = (num: number) => (num < 10 ? `0${num}` : num)
+
+export const getOrderedTasks = (tasks: Task[]): Task[] => {
+  // Sort tasks by status and then by date
+  return tasks.sort((a, b) => {
+    if (a.status === b.status) {
+      return a.createdAt > b.createdAt ? -1 : 1
+    }
+
+    return a.status > b.status ? -1 : 1
+  })
+
+  // Sort tasks by status and then by date
+}
