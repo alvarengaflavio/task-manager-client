b1be360577fc1cf7f6e01f168276d84a087f900b
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-25 22:36:05 -0300

    feat(TaskItem): add TaskStatus State

--- src/components/shared/TaskItem/StyledTaskItem.ts
+++ src/components/shared/TaskItem/StyledTaskItem.ts
@@ -1,13 +1,19 @@
 import styled from 'styled-components'
 
 export const StyledTaskItem = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
   padding: 10px;
+  margin: 0 auto;
   border-bottom: 1px solid #ccc;
-  cursor: pointer;
+  border-radius: 9px;
+  height: 9rem;
+  max-width: 50rem;
+  min-width: calc(50rem);
+  width: 90%;
 
   transition: background-color 0.2s ease-in-out;
 
@@ -15,5 +21,80 @@ export const StyledTaskItem = styled.div`
 
   &:hover {
     background-color: ${({ theme }) => theme.colors.interactive};
+  }
+
+  div.text--wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-self: flex-start;
+    gap: 0.3rem;
+    width: 93%;
+    cursor: pointer;
+
+    h4 {
+      font-weight: 500;
+      color: ${({ theme }) => theme.colors.text};
+    }
+
+    p {
+      font-weight: 300;
+      width: 100%;
+      text-align: center;
+    }
+
+    p:first-of-type {
+      height: 70%;
+      min-height: 4.5rem;
+      font-weight: 300;
+      line-height: 1.5rem;
+      font-size: var(--font-size-md);
+    }
+
+    p:last-child {
+      font-size: var(--font-size-sm);
+      height: 1rem;
+    }
+  }
+
+  div.status--wrapper {
+    display: flex;
+    flex-direction: column;
+    /* align-items: center; */
+    /* justify-content: center; */
+
+    position: absolute;
+    top: calc(50% - 3.5rem);
+    right: 1rem;
+
+    button {
+      border-radius: 50%;
+      border: 1px solid transparent;
+      margin: 0.2rem 0;
+      width: 2rem;
+      height: 2rem;
+
+      color: var(--color-disabled-text);
+      transition: background-color 0.3s ease-in-out, color 0.3s ease-in-out;
+
+      cursor: pointer;
+      z-index: 5;
+    }
+
+    button:hover {
+      color: var(--color-white);
+      border-color: ${({ theme }) => theme.colors.backgroundVar};
+      background-color: ${({ theme }) => theme.colors.interactive};
+    }
+
+    button.active {
+      color: var(--color-white);
+      border-color: var(--color-white);
+      background-color: ${({ theme }) => theme.colors.interactive};
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    min-width: 40rem;
   }
 `
--- src/components/shared/TaskItem/index.tsx
+++ src/components/shared/TaskItem/index.tsx
@@ -1,8 +1,10 @@
 import { doubleDigit } from '@/utils/functions.util'
-import { Task } from '@/utils/types/tasks'
+import { Task, TaskStatus } from '@/utils/types/tasks'
+import { useState } from 'react'
 import { StyledTaskItem } from './StyledTaskItem'
 
 export function TaskItem(props: Props) {
+  const [status, setStatus] = useState<TaskStatus>(props.status)
   const date = new Date(props.createdAt)
 
   const showDate = `${date.getDay()}/${
@@ -10,12 +12,38 @@ export function TaskItem(props: Props) {
   }/${date.getFullYear()}`
   const showTime = `${date.getHours()}:${doubleDigit(date.getMinutes())}`
 
+  const handleStatus = (status: TaskStatus) => {
+    setStatus(status)
+  }
+
   return (
     <StyledTaskItem>
-      <p>{props.status}</p>
-      <h4>{props.title}</h4>
-      <p>{props.description}</p>
-      <p>{`${showDate} - ${showTime}`}</p>
+      <div className="text--wrapper">
+        <h4>{props.title.toLocaleUpperCase()}</h4>
+        <p>{props.description}</p>
+        <p>{`${showDate} - ${showTime}`}</p>
+      </div>
+
+      <div className="status--wrapper">
+        <button
+          onClick={() => handleStatus('OPEN')}
+          className={status === 'OPEN' ? 'active' : ''}
+        >
+          O
+        </button>
+        <button
+          onClick={() => handleStatus('IN_PROGRESS')}
+          className={status === 'IN_PROGRESS' ? 'active' : ''}
+        >
+          P
+        </button>
+        <button
+          onClick={() => handleStatus('DONE')}
+          className={status === 'DONE' ? 'active' : ''}
+        >
+          D
+        </button>
+      </div>
     </StyledTaskItem>
   )
 }
