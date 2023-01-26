f868668f22ce5d0aeca462492c68bf8d2ffbd373
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-26 18:36:13 -0300

    feat(app): add TaskPage Route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { AboutPage } from './pages/AboutPage'
 import { HomePage } from './pages/Home'
 import { LoginPage } from './pages/LoginPage'
 import { RegisterPage } from './pages/RegisterPage'
+import { TaskPage } from './pages/TaskPage'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/home" element={<HomePage />} />
+            <Route path="/task/:id" element={<TaskPage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="*" element={<HomePage />} />
           </Routes>
