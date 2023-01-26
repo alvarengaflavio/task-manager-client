686a11c64e183f611e81fd82f9e106aa41a4a4c1
Flavio Alvarenga <flavio.alva@outlook.com>
2023-01-25 21:13:27 -0300

    feat(taskItem): fix date

--- src/components/shared/TaskItem/index.tsx
+++ src/components/shared/TaskItem/index.tsx
@@ -1,13 +1,21 @@
+import { doubleDigit } from '@/utils/functions.util'
 import { Task } from '@/utils/types/tasks'
 import { StyledTaskItem } from './StyledTaskItem'
 
 export function TaskItem(props: Props) {
+  const date = new Date(props.createdAt)
+
+  const showDate = `${date.getDay()}/${
+    date.getMonth() + 1
+  }/${date.getFullYear()}`
+  const showTime = `${date.getHours()}:${doubleDigit(date.getMinutes())}`
+
   return (
     <StyledTaskItem>
       <p>{props.status}</p>
       <h4>{props.title}</h4>
       <p>{props.description}</p>
-      <p>{props.createdAt}</p>
+      <p>{`${showDate} - ${showTime}`}</p>
     </StyledTaskItem>
   )
 }
